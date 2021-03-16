85814998a1f09034cb1f3282a9f6c7b379b1a576
Sherrod Blankner <mooncalfskb@mooncalfskb.local>
2021-03-16 00:57:04 -0700

    trying to see if host is mysql

--- scrapers/ca/download.py
+++ scrapers/ca/download.py
@@ -121,15 +121,15 @@ def intialize_db_passwords():
             
             cursor.execute(update_root_grant)
             cursor.execute("flush privileges;")
+            '''
 
-            create_skopos_user = "CREATE USER '" + skopos_user + "'@'%' IDENTIFIED BY '" + skopos_password + "';"
+            create_skopos_user = "CREATE USER '" + skopos_user + "'@`mysql` IDENTIFIED BY '" + skopos_password + "';"
             print(create_skopos_user)
 
             cursor.execute(create_skopos_user)
             cursor.execute("flush privileges;")
-            '''
 
-            update_skopos_grant = "GRANT ALL PRIVILEGES ON *.* TO '" + skopos_user + "'@`localhost`;"
+            update_skopos_grant = "GRANT ALL PRIVILEGES ON *.* TO '" + skopos_user + "'@`mysql`;"
             print(update_skopos_grant)
             
             cursor.execute(update_skopos_grant)
